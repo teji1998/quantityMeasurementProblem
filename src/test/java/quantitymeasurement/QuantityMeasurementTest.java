e2184bcb398787a2203edfc33653ece15124fb9a
“teji1998” <tejasvinirpk@gmail.com>
2020-09-05 04:57:36 +0530

    Added:Test case and code for zero feet and one feet to be not equal when compared

--- src/test/java/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/quantitymeasurement/QuantityMeasurementTest.java
@@ -9,9 +9,16 @@ import org.mockito.junit.MockitoJUnitRunner;
 public class QuantityMeasurementTest {
 
 	@Test
-	public void givenZeroFeetAndZeroFeet_WhenComparedInLengths_ShouldReturnEqual(){
+	public void givenZeroFeetAndZeroFeet_WhenComparedInLengths_ShouldReturnEquals(){
 		Feet feet1 = new Feet(0.0);
 		Feet feet2 = new Feet(0.0);
 		Assert.assertEquals(feet1, feet2);
+	}
+
+	@Test
+	public void givenZeroFeetAndOneFeet_WhenComparedInLengths_ShouldReturnNotEquals(){
+		Feet feet1 = new Feet(0.0);
+		Feet feet2 = new Feet(1.0);
+		Assert.assertNotEquals(feet1, feet2);
 	}
 }
