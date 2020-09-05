567e9e8b5fdd1c86632b1c7f9ccba01ac29987e5
“teji1998” <tejasvinirpk@gmail.com>
2020-09-05 05:32:39 +0530

    Added:Test cases for null check of feet unit

--- src/test/java/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/quantitymeasurement/QuantityMeasurementTest.java
@@ -5,20 +5,36 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.Objects;
+
 @RunWith(MockitoJUnitRunner.class)
 public class QuantityMeasurementTest {
 
 	@Test
-	public void givenZeroFeetAndZeroFeet_WhenComparedInLengths_ShouldReturnEquals(){
+	public void givenZeroFeetAndZeroFeet_WhenComparedInLengths_ShouldReturnEquals() {
 		Feet feet1 = new Feet(0.0);
 		Feet feet2 = new Feet(0.0);
 		Assert.assertEquals(feet1, feet2);
 	}
 
 	@Test
-	public void givenZeroFeetAndOneFeet_WhenComparedInLengths_ShouldReturnNotEquals(){
+	public void givenZeroFeetAndOneFeet_WhenComparedInLengths_ShouldReturnNotEquals() {
 		Feet feet1 = new Feet(0.0);
 		Feet feet2 = new Feet(1.0);
 		Assert.assertNotEquals(feet1, feet2);
+	}
+
+	@Test
+	public void givenFeet_WhenIsNotNull_ShouldReturnTrue() {
+		Feet feet1 = new Feet(1.0);
+		boolean nullValueCheck = Objects.nonNull(feet1);
+		Assert.assertTrue(nullValueCheck);
+	}
+
+	@Test
+	public void givenFeet_WhenIsNull_ShouldReturnTrue() {
+		Feet feet1 = null;
+		boolean nullValueCheck = Objects.isNull(feet1);
+		Assert.assertTrue(nullValueCheck);
 	}
 }
